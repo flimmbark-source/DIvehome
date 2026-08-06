92ec061d5ac99d658b4fbdc4982f24eac1bfd20c
flimmbark-source <flimmbark@gmail.com>
2026-08-06 17:44:08 +0300

    Let enemies continue through the player plane

--- src/game/world.js
+++ src/game/world.js
@@ -2,18 +2,8 @@ import { APPARATUS_CONFIG, tunnelAxisAt } from './config.js'
 import { patternOffset } from './simulation.js'
 
 export function enemyWorldPosition(enemy, run, config = APPARATUS_CONFIG) {
-  const parked = enemy.state === 'parked'
-  const distanceAhead = parked ? config.INTERACTION_DISTANCE : enemy.routeZ - run.travelDistance
+  const distanceAhead = enemy.routeZ - run.travelDistance
   const offset = patternOffset(enemy, distanceAhead, run.elapsed)
-
-  if (parked) {
-    return {
-      x: offset.x * config.TUBE_RADIUS,
-      y: offset.y * config.TUBE_RADIUS,
-      z: -config.INTERACTION_DISTANCE,
-    }
-  }
-
   const enemyAxis = tunnelAxisAt(enemy.routeZ, config)
   const currentAxis = tunnelAxisAt(run.travelDistance, config)
   return {
